a1de28c33c092c32133644f05d9e8cf323dd5bc2
Neil Snyder <neil.r.snyder@gmail.comwq>
2025-01-19 11:25:48 -0800

    Write out job duties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { filter } from 'rxjs/operators';
+import { LayoutService } from './services/layout.service';
+import { HeaderService } from './services/header.service';
+import { StyleUpdateService } from './services/style-update.service';
 
 @Component({
 	selector: 'app-root',
@@ -33,8 +36,20 @@ export class AppComponent implements OnInit {
 					pend: '2021' }
 			], 
 			duties: [
-				'duty1', 
-				'duty2'
+				{ desc: 'Migrated reporting server backend to redundant multi-server architecture', 
+				  techs: ['Apache','PHP','JavaScript','MySQL']}, 
+				{ desc: 'Developed failover system for critical photolithography databases, ensuring seamless operations during network outtages', 
+				  techs: ['PHP', 'JavaScript', 'MySQL']}, 
+				{ desc: 'Built interactive fab map with live tool-state data, improving factory visualization and navigation',
+				  techs: ['JavaScript', 'AngularJS', 'PHP', 'SVG', 'MySQL']}, 
+				{ desc: 'Optimized reporting tools, reducing page load times by over 95%, saving 500+ hours per month in productivity', 
+				  techs: ['PHP', 'JavaScript', 'MySQL']}, 
+				{ desc: 'Refactored PHP codebase to implement parameterized queries, enhacning security against SQL injection attacks'}, 
+				{ desc: 'Deployed SSL encryption and transitioned static user permissions to a dynamic role-based system'}, 
+				{ desc: 'Designed and implemented an advanced PTO calendar system, integrating LDAP for real-time employee data updates and enabling supervisors to manage schedules, approve PTO requests, and assign quality points through an intuitive interface',
+				  techs: ['PHP', 'JavaScript', 'AngularJS', 'MySQL']}, 
+				{ desc: 'Developed a headcount view for HR, providing a dynamic pivot table for tracking employee assignments across factory zones and shifts, significantly improving operational visibility and decision-making.', 
+				  techs: ['PHP', 'JavaScript', 'AngularJS']}
 			]
 		}, 
 		{
@@ -50,8 +65,8 @@ export class AppComponent implements OnInit {
 					pend: '2017' }
 			], 
 			duties: [
-				'duty1', 
-				'duty2'
+				{ desc: 'Built networked inventory tracking system, eliminating concurrency issues and automating expense reimbursement invoicing process, saving $10,000 in expenses (MS Access, VBA)'}, 
+				{ desc: 'Coordinated agreements and communications for 5-10 clients simultaenously, ensuring efficient project execution'}
 			]
 		}, 
 		{
@@ -67,8 +82,8 @@ export class AppComponent implements OnInit {
 					pend: '2010' }, 
 			], 
 			duties: [
-				'duty1', 
-				'duty2'
+				{ desc: 'Developed dynamic named array reference system for financial models, enabling seamless data updates and ipmroving quarterly reporting workflow (MS Excel, VBA)'}, 
+				{ desc: 'Integrated client database with call-trakcing software to streamline workflows (MS Access, VBA)'}
 			]
 		}
 	];
